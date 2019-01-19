6e97adda2649d39c8532023eaadf9f96125e48fa
Юлия Бордачева <s_proza@list.ru>
2019-01-19 11:40:28 +0300

    '5c'

--- visa.php
+++ visa.php
@@ -1,23 +1,23 @@
-<?php 
+<?php
 
-
-
-
-$resource = fopen(__DIR__ . DIRECTORY_SEPARATOR . 'opendata.csv', 'r');
-
+$resource = fopen(__DIR__ . DIRECTORY_SEPARATOR . 'opendata.csv', 'r'
+);
 if (!$resource) {
-	echo 'Cant read file';
-	exit;
+echo 'Cant read file';
+exit;
 }
-	$row = 0;
-    while ($data = fgetcsv($resource, 0, ';')) {
-        $row++;
-        echo "Row #" . $row . " contains " . count($data) . " fields" . PHP_EOL;
-        $strData = print_r($data, true);
-        $encodedStrData = iconv('CP1251', 'UTF-8', $strData);
-        print_r($encodedStrData);
-        echo PHP_EOL;
-    }
+
+$row = 0;
+while ($data = fgetcsv($resource, 0, ',')) {
+    $row++;
+    echo "Row #" . $row . " contains " . count($data) . " fields" . PHP_EOL;
+    print_r($data);
+    echo PHP_EOL;
+
+
+}
+
+
 
 
 
